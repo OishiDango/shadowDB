936c691a52e7a72af7be98f4b7b8b6f28f0f06e7
User <kohjianhao@gmail.com>
2025-07-18 22:17:29 +1000

    Connect to Render PostgreSQL using DATABASE_URL

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,7 @@
+# db.py
 import psycopg2
+import os
 
 def get_connection():
-    return psycopg2.connect(
-        dbname="sqli_demo",
-        user="postgres",
-        password="123456",
-        host="localhost",
-        port="5432"
-    )
+    DATABASE_URL = os.environ.get("DATABASE_URL")
+    return psycopg2.connect(DATABASE_URL)
